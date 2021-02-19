7516571510f48dfc3acd831d94b1baa67fa4367e
Rida Aftab <aftabr@sheridancollege.ca>
2021-02-19 10:48:28 -0500

    Created a UI for the Cupcake Shop.

--- Cupcake/Form1.Designer.cs
+++ Cupcake/Form1.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cupcakeName = new System.Windows.Forms.TextBox();
+            this.cupcakePrice = new System.Windows.Forms.TextBox();
+            this.cupcakeDate = new System.Windows.Forms.TextBox();
+            this.cupcakeModified = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(219, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Cupcake Shop";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cupcake Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cupcake Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(320, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cupcake Created Date:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(390, 31);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cupcake Last Modified Date:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(315, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 69);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Done!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cupcakeName
+            // 
+            this.cupcakeName.Location = new System.Drawing.Point(272, 105);
+            this.cupcakeName.Name = "cupcakeName";
+            this.cupcakeName.Size = new System.Drawing.Size(163, 20);
+            this.cupcakeName.TabIndex = 6;
+            // 
+            // cupcakePrice
+            // 
+            this.cupcakePrice.Location = new System.Drawing.Point(263, 152);
+            this.cupcakePrice.Name = "cupcakePrice";
+            this.cupcakePrice.Size = new System.Drawing.Size(172, 20);
+            this.cupcakePrice.TabIndex = 7;
+            // 
+            // cupcakeDate
+            // 
+            this.cupcakeDate.Location = new System.Drawing.Point(358, 194);
+            this.cupcakeDate.Name = "cupcakeDate";
+            this.cupcakeDate.Size = new System.Drawing.Size(172, 20);
+            this.cupcakeDate.TabIndex = 8;
+            // 
+            // cupcakeModified
+            // 
+            this.cupcakeModified.Location = new System.Drawing.Point(413, 232);
+            this.cupcakeModified.Name = "cupcakeModified";
+            this.cupcakeModified.Size = new System.Drawing.Size(116, 20);
+            this.cupcakeModified.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(535, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 233);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(744, 330);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cupcakeModified);
+            this.Controls.Add(this.cupcakeDate);
+            this.Controls.Add(this.cupcakePrice);
+            this.Controls.Add(this.cupcakeName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox cupcakeName;
+        private System.Windows.Forms.TextBox cupcakePrice;
+        private System.Windows.Forms.TextBox cupcakeDate;
+        private System.Windows.Forms.TextBox cupcakeModified;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
